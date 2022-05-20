b01e985b9200f8a3b50f9439adeb2115dd001130
Kenan Bouvier <kenan@kali>
2022-05-20 03:52:25 +0100

    config update

---
+++ php/config/keys.php
@@ -0,0 +1,6 @@
+<?php
+    define( 'TWITTER_CONSUMER_KEY', '<KEY>' );
+    define( 'TWITTER_CONSUMER_SECRET', '<KEY>' );
+    define( 'TWITTER_ACCESS_TOKEN', '<KEY>' );
+    define( 'TWITTER_ACCESS_TOKEN_SECRET', '<KEY>' );
+?>
