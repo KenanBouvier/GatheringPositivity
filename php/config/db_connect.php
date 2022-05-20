989c38eb271c0cb32602e27a43a4715fd9e6eeb4
Kenan Bouvier <kenan@kali>
2022-05-20 03:48:56 +0100

    config replace

--- php/config/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $conn = mysqli_connect('localhost','root','Gs]JHmn0tcxNiles','gatheringpositivity');
-
-    if(!$conn){
-        echo 'Connection error: ' . mysqli_connect_error();
-    }
-?>
